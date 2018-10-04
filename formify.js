f4217cb9719401b9a8c4167dcc19ab09b545495b
Marcus Wiseman <marcuswiseman@evolutionfunding.com>
2018-10-04 08:38:25 +0100

    Comments

--- formify.js
+++ formify.js
@@ -1,14 +1,19 @@
-var formify = {
-	loader: null
-};
+var formify = {};
+formify.settings = {showLoader: false};
+formify.loader = null;
 
+/*
+ * Reset timer and remove loader from screen.
+ */
 formify.clear = function () {
 	clearTimeout(formify.loader);
 	if ($('.js-formify-loader').length) {
 		$('.js-formify-loader').remove();
 	}
 }
-
+/*
+ * Start timer. (Once loader is shown, after some time, remove it)
+ */
 formify.timeoutLoader = function (miliseconds = 2500) {
 	formify.loader = setTimeout(function () {
 		if ($('.js-formify-loader').length) {
@@ -19,7 +24,14 @@ formify.timeoutLoader = function (miliseconds = 2500) {
 
 $(function () {
 
-	$('input, select, textarea').change(function (e) {
+	/*
+ 	 * Bind two inputs of two forms (or more) together.
+	 * Example:
+	 *  - form1 has "<form data-action='form1'><input name='a'></form>"
+	 *  - and form2 has "<form data-action='form2'><input name='b' data-bind='form1.a'></form>"
+	 * When Input[name='a'] changes, it updates Input[name='b'] with the same value.
+ 	 */
+	$('.formify input, .formify select, .formify textarea').change(function (e) {
 		var $el = $(this);
 		$('.formify').each(function (i, v) {
 			if ($(v).attr('data-action')) {
@@ -36,18 +48,24 @@ $(function () {
 
 	});
 
+	/*
+ 	 * Handle submit event for forms classed with .formify and submit form data through ajax.
+	 * Handle response and if callback is returned then attempt to call named function prefixed do_.
+	 * TODO :- Test file uploading.
+	 */
 	$(document).on('submit', '.formify', function (e) {
 
 		e.preventDefault();
 
-
 		formify.clear();
 
-		var style = "position:fixed;right:20px;top:0;z-index:9999;padding:5px 10px;";
-		style += "background-color:#eaa323;color:white;border-radius: 0px 0px 8px 8px";
-		var loader = '<div class="js-formify-loader" style="' + style + '">';
-		loader += 'Loading...</div>';
-		$('body').append(loader);
+		if (formify.settings.showLoader) {
+			var style = "position:fixed;right:20px;top:0;z-index:9999;padding:5px 10px;";
+			style += "background-color:#eaa323;color:white;border-radius: 0px 0px 8px 8px";
+			var loader = '<div class="js-formify-loader" style="' + style + '">';
+			loader += 'Loading...</div>';
+			$('body').append(loader);
+		}
 
 		var $this = $(this);
 		$($this).find('input[type="submit"]').prop('disabled', 'disabled');
@@ -61,26 +79,30 @@ $(function () {
 			contentType: "multipart/form-data"
 		})
 
-		.done(function (data) {
-			if (typeof(data.callback) !== "undefined") {
-				if (typeof(window['do_' + data.callback]) === 'function') {
-					window['do_' + data.callback](data.arguments);
+			.done(function (data) {
+				if (typeof(data.callback) !== "undefined") {
+					if (typeof(window['do_' + data.callback]) === 'function') {
+						window['do_' + data.callback](data.arguments);
+					} else {
+						console.log('Callback given but could not find client-side function to execute.');
+					}
 				} else {
-					console.log('Callback given but could not find client-side function to execute.');
+					console.log('No callback specified: In ajax response, return json -> { callback:"functionname", arguments:[...], ... }');
 				}
-			} else {
-				console.log('No callback specified: In ajax response, return json -> { callback:"functionname", arguments:[...], ... }');
-			}
-			$('.js-formify-loader').html('Success').css('background-color', '#3dba4c');
-			formify.timeoutLoader();
-			$($this).find('input[type="submit"]').prop('disabled', '');
-		})
+				if (formify.settings.showLoader) {
+					$('.js-formify-loader').html('Success').css('background-color', '#3dba4c');
+					formify.timeoutLoader();
+				}
+				$($this).find('input[type="submit"]').prop('disabled', '');
+			})
 
-		.fail(function (data) {
-			$('.js-formify-loader').html('Error').css('background-color', '#ba3d3d');
-			formify.timeoutLoader();
-			$($this).find('input[type="submit"]').prop('disabled', '');
-		});
+			.fail(function (data) {
+				if (formify.settings.showLoader) {
+					$('.js-formify-loader').html('Error').css('background-color', '#ba3d3d');
+					formify.timeoutLoader();
+				}
+				$($this).find('input[type="submit"]').prop('disabled', '');
+			});
 
 	});
 });
